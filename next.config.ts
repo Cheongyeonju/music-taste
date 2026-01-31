ca0f51f5c51f5afa1deb637ae58053d874c99fa8
Milkyway <milkyway@Milkywayui-MacBookAir.local>
2026-01-31 09:12:08 +0900

    tw 0131

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,7 @@
-import type { Config } from "tailwindcss";
+import type { NextConfig } from "next";
 
-const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Pretendard", "sans-serif"], // 여기서도 폰트 지정
-      },
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+const nextConfig: NextConfig = {
+  /* config options here */
 };
-export default config;
+
+export default nextConfig;
